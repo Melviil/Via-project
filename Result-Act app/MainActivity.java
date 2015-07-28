a40ecd7845404495ebc6555fbf1a58e20da0437d
davidkocnar <davidkocnar@seznam.cz>
2015-07-28 23:53:17 +0200

    Result Act app

---
+++ Result-Act app/MainActivity.java
@@ -0,0 +1,56 @@
+package cz.davidkocnar.multipleactivityapp;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("cz.davidkocnar.multipleactivityapp"); //if it's a string you stored.
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(value);
+
+       
+
+        Toast to = Toast.makeText(getApplicationContext(), "onCreate! MAIN", Toast.LENGTH_LONG);
+        to.show();
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                //intent.putExtra(EXTRA_MESSAGE, message);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK){
+                String result=data.getStringExtra("result");
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //Write your code if there's no result
+            }
+        }
+    }//onActivityResult
+
+
+}
